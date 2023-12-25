5c7db4c2359d9d59b8d8252bda7c9b97f4bb1e62
AyxanAmirov <amirovayxan0@gmail.com>
2023-12-25 07:56:43 -0800

    Task-20 new

--- task-20/app.js
+++ task-20/app.js
@@ -1,18 +1,13 @@
 // task-1
 // console.log('My name is Aykhan');
 
-
 // task-2
 // let user_name = 'Aykhan'
 // console.log(user_name)
 
-
-
 // task-3
 // person
 // $add
-
-
 
 // task-4
 // let book_price =  750
@@ -20,47 +15,35 @@
 // let total_price = book_price * book_amount
 // console.log(total_price)
 
-
-
 // task-5
 // let kmh_speed = 75;
 // let ms_speed = kmh_speed * 1000 / 3600
 // console.log(ms_speed)
-
-
 
 // task-6
 // let age = Number(prompt('Doğum Tarixinizi Girin'))
 // let day = (2024 - age) * 365
 // alert(day)
 
-
-
 // task-7
 // let num = 14
 // let calc = (num ** 5) / 3
 // console.log(calc)
-
-
 
 // task-8
 // let  first_name = prompt("sizin adınız:")
 // let last_name = prompt("sizin soy adınız:")
 // alert('Sizin adınız:' + " " + first_name + " " + last_name + " Tanışlığımıza çox şadam!")
 
-
 // task-9
 // let first_name = "Murad"
 // let last_name = "Orucov"
 // alert("Sizi bir daha görməyimizə şadıq:" + " " + first_name + " " +last_name)
 
-
-
 // task-10
 
 // let user_name = prompt("Aınızı daxil edin:")
 // alert(user_name.length)
-
 
 // task-11
 
@@ -81,22 +64,16 @@
 // let user_name = prompt("Salam (adInız)!")
 // alert("Salam " + user_name + "!")
 
-
-
 // task-14
 // let num_1 = Number(prompt('1-ci Rəqəmi daxil edin'))
 // let num_2 = Number(prompt('2-ci Rəqəmi daxil edin'))
 // console.log((num_1 + num_2) + "\r\n"+( num_1 - num_2) +"\r\n"+ (num_1 * num_2) +"\r\n" + (num_1 / num_2))
 
-
-
-
 // task-15
 // let num_1 = 12345 ;
-// let num_2 = 98765 ; 
+// let num_2 = 98765 ;
 // let total = String((num_1 * num_2 ));
 // console.log(total.charAt(5))
-
 
 // task-16
 // "" + 1 + 0 = "1"
@@ -113,8 +90,6 @@
 // null + 1 = 1
 // undefined + 1 =NaN
 
-
-
 // task-17
 
 // let num_1 = Number(prompt("dörd rəqəmli ədəd daxil et:"))
@@ -122,35 +97,25 @@
 // let total = num_1 + num_2
 // console.log(total)
 
-
-
 // taks-18
-
 
 // let user_name = prompt("Adınızı daxil edin:")
 // let last_name = prompt("Soy adınızı daxil edin:")
 // alert("Salam " + user_name + "!")
 
-
 // task-19
- 
+
 // let price = prompt("Qiyməti daxil edin:")
 // ??
 
-
 // task-20
-
 
 // let price = Number(prompt("Maaşınızı daxil edin:"))
 // alert("Vergisiz maaşınız:" + ( price + (price * 13 / 100) ) )
 
-
 //task-21
 // let name = prompt("ad,soyad,ata ad; daxil edin:")
 // alert(name)
-
-
-
 
 // task-24
 // let age = prompt("yaşınızı daxil edin:")
@@ -160,7 +125,26 @@
 //     alert("Access granted")
 // }
 
+//task-27
+// let id = prompt("enter product id:");
+// switch (id) {
+//   case "1":
+//     alert("Available 10 pcs.");
+//     break;
 
+//   case "2":
+//     alert("Available 256 pcs.");
+//     break;
+//   case "3":
+//     alert("Available 53 pcs.");
+//     break;
+//   case "4":
+//     alert("There are 3 available.");
+//     break;
+//   default:
+//     alert("Out of stock");
+//     break;
+// }
 
 // task-28
 // let gender = prompt("Cinsiyyətinizi daxil edin (M/F) :")
@@ -172,4 +156,49 @@
 // }
 // else{
 //     alert("Cinsiyyətinizi düzgün qeyd edin")
+// }
+
+
+//task-29
+// let id = prompt("Ay-ı daxil edin:");
+// switch (id) {
+//   case "1":
+//     console.log("Yanvar");
+//     break;
+
+//   case "2":
+//     console.log("Fevral");
+//     break;
+//   case "3":
+//     console.log("Mart");
+//     break;
+//   case "4":
+//     console.log("Aprel");
+//     break;
+//   case "5":
+//     console.log("May");
+//     break;
+//     case "6":
+//     console.log("Iyun");
+//     break;
+//     case "7":
+//     console.log("Iyul");
+//     break;
+//     case "8":
+//     console.log("Avqust");
+//     break;
+//     case "9":
+//     console.log("Sentyabr");
+//     break;
+//     case "10":
+//     console.log("Oktyabr");
+//     break;
+//     case "11":
+//     console.log("Noyabr");
+//     break;
+//     case "12":
+//     console.log("Dekabr");
+//     break;
+//     default:
+//         alert("Düzgün seçim edin")
 // }
