3defd48bb0c4d44c997666d9959d147d4c8909c8
AyxanAmirov <amirovayxan0@gmail.com>
2024-01-12 13:41:56 -0800

    task-22

---
+++ task-22/app.js
@@ -0,0 +1,50 @@
+//task-1
+
+// const firstElement = document.querySelector("h1")
+// console.log(firstElement);
+
+
+//task-2
+
+// const heading = document.querySelector("h1")
+// const body = document.querySelector("body")
+// heading.remove()
+
+// const text = document.createElement("p")
+// text.textContent = "TITAN RTX"
+// body.appendChild(text)
+
+              //task-2: versiya-2
+
+// const heading = document.querySelector("h1")
+// const list = document.querySelector("ul")
+// heading.remove()
+// const text = document.createElement("p")
+// text.textContent = "TITAN RTX"
+//  list.appendChild(text)
+
+
+//task-3
+
+// let primary = document.querySelectorAll(".primary")
+// primary.forEach((listElementPrimary) => listElementPrimary.setAttribute("style","color : #ebebeb"))
+
+//task-4
+
+//  let primary = document.querySelectorAll(".primary")
+// for (let i = 0; i <= primary.length-1; i++) {
+//     const primaries = primary[i];
+//     console.log(primaries);
+// }
+
+
+//task-5
+
+// let allListElements = document.querySelectorAll("li")
+// allListElements.forEach(function(listElements){
+//     if(listElements.className !== "primary"){
+//         listElements.classList.add("secondary")
+//     }
+//     listElements.classList.add("item")
+//     console.log(listElements);
+// })
